e89f70f6d8d5fe11ba21f0c45948302f06619c81
HarshaAK11 <harshaadithyakumar@gmail.com>
2025-05-07 11:55:19 +0530

    CORS settings updated

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://harsha.vercel.app'], 
+    origin: ['http://localhost:5173', 'https://harsha.vercel.app', 'https://harsha-8pt1ommk5-harsha-aks-projects.vercel.app'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
